b6b9e1d99eed2c3b9d0f7e0df559f387044287c5
John James Jacoby <johnjamesjacoby@me.com>
2016-01-14 11:21:58 -0600

    Skip schema check; let LudicrousDB manage this for us.

--- wp-site-cloner.php
+++ wp-site-cloner.php
@@ -191,14 +191,11 @@ final class WP_Site_Cloner {
 		$from_site_prefix_length = strlen( $this->from_site_prefix );
 		$from_site_prefix_like   = $wpdb->esc_like( $this->from_site_prefix );
 
-		// @todo Needs Ludicrous DB support
-		$schema = DB_NAME;
-
 		// Get sources Tables
 		if ( $this->from_site_id === (int) get_current_site()->blog_id ) {
 			$from_site_tables = $this->get_primary_tables( $this->from_site_prefix );
 		} else {
-			$sql_query        = $wpdb->prepare( 'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = \'%s\' AND TABLE_NAME LIKE \'%s\'', $schema, $from_site_prefix_like . '%' );
+			$sql_query        = $wpdb->prepare( 'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME LIKE \'%s\'', $from_site_prefix_like . '%' );
 			$from_site_tables = $this->do_sql_query( $sql_query, 'col' );
 		}
 
@@ -210,10 +207,10 @@ final class WP_Site_Cloner {
 			$this->do_sql_query( 'DROP TABLE IF EXISTS `' . $table_name . '`' );
 
 			// Create new table from source table
-			$this->do_sql_query( 'CREATE TABLE IF NOT EXISTS `' . $table_name . '` LIKE `' . $schema . '`.`' . $table . '`' );
+			$this->do_sql_query( 'CREATE TABLE IF NOT EXISTS `' . $table_name . '` LIKE `' . $table . '`' );
 
 			// Populate database with data from source table
-			$this->do_sql_query( 'INSERT `' . $table_name . '` SELECT * FROM `' . $schema . '`.`' . $table . '`' );
+			$this->do_sql_query( 'INSERT `' . $table_name . '` SELECT * FROM `' . $table . '`' );
 		}
 	}
 
